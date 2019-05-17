0f59534fe2ef4bbc8d78d5a444f8cd13be50a09e
Algeneralo <obayda.alesawi@gmail.com>
2019-05-17 18:15:35 +0300

    create directors migration

---
+++ database/migrations/2019_05_17_151306_create_directors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDirectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('directors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name_ar');
+            $table->string('name_en');
+            $table->string('position_ar');
+            $table->string('position_en');
+            $table->string('image');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('directors');
+    }
+}
