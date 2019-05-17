1b797ecd3733ee1d9f279a7dc8deb95a8ac943a0
Algeneralo <obayda.alesawi@gmail.com>
2019-05-17 18:36:10 +0300

    create models

---
+++ app/FinancePrograms.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use App\Scopes\LangScope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FinancePrograms extends Model
+{
+    use SoftDeletes;
+    protected $table = "posts";
+    protected $fillable = ['title', 'body', 'image', 'lang'];
+    protected $attributes = [
+        'type' => 4,
+    ];
+
+    /**
+     * The "booting" method of the model.
+     * Get the news for the current language
+     * The second scope is for getting only the news form posts table
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new LangScope());
+        static::addGlobalScope('type', function (Builder $builder) {
+            $builder->where('type', 4);
+        });
+    }
+}
---
+++ app/News.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use App\Scopes\LangScope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class News extends Model
+{
+    use SoftDeletes;
+    protected $table = "posts";
+    protected $fillable = ['title', 'body', 'image', 'lang'];
+    protected $attributes = [
+        'type' => 1,
+    ];
+
+    /**
+     * The "booting" method of the model.
+     * Get the news for the current language
+     * The second scope is for getting only the news form posts table
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new LangScope());
+        static::addGlobalScope('type', function (Builder $builder) {
+            $builder->where('type', 1);
+        });
+    }
+}
