96b03ee322ac2a391da6bec1518196cf53f208c7
Algeneralo <obayda.alesawi@gmail.com>
2019-05-18 01:30:10 +0300

    create models

--- app/Slider.php
+++ app/Slider.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
 {
-    protected $fillable = ['image', 'caption'];
+    protected $fillable = ['image', 'caption_ar', 'caption_en'];
 }
