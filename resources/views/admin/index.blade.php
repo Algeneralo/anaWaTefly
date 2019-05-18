372e365cbbe003361f7dd636146c42c5bcb3b660
Algeneralo <obayda.alesawi@gmail.com>
2019-05-19 00:35:35 +0300

    remove flush session message

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,31 +8,6 @@
     </style>
 @endsection
 @section('body')
-    @if($errors->any())
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                        aria-hidden="true">×</span>
-            </button>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </div>
-    @endif
-    @if(session()->has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                        aria-hidden="true">×</span>
-            </button>
-            {{session('success')}}
-        </div>
-    @elseif(session()->has('error'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                        aria-hidden="true">×</span>
-            </button>
-            {{session('error')}}
-        </div>
-    @endif
     <form action="/admin/config/{{$config->id??''}}" method="post" class="m-b-30">
         @csrf
         <div class="card-box"><h4 class="header-title m-t-0 mb-3">الرسالة الترحيبية</h4>
