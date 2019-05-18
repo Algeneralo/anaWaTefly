1526eb80e1319d7b325ce077815f46cee9708c06
Algeneralo <obayda.alesawi@gmail.com>
2019-05-19 00:33:57 +0300

    create specific controller for admin about page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,36 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\config;
-use App\Http\Requests\StoreConfigRequest;
-use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    /**
-     * get the site configuration
-     *
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function index()
-    {
-        $config = config::first();
-        return view('admin.index', compact('config'));
-    }
-
-    /**
-     * Update the site configuration if exist or crate new one then return the status
-     *
-     * @param StoreConfigRequest $request
-     * @param null $id
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function configStore(StoreConfigRequest $request, $id = null)
-    {
-        //find the configuration by id or create new one
-        $status = config::updateOrCreate(['id' => $id,], $request->all());
-        if ($status)
-            return redirect()->back()->with('success', 'تم التعديل بنجاح');
-        return redirect()->back()->with('error', 'حدث خلل,يرجى المحاولة فيما بعد');
-    }
 }
