4a560d756d73501602ff9fd0de6ebb7f623217cf
Algeneralo <obayda.alesawi@gmail.com>
2019-05-18 23:16:36 +0300

    edit table name

--- app/Config.php
+++ app/Config.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class config extends Model
 {
     use SoftDeletes;
+    protected $table = "config";
     protected $fillable = ['facebook', 'twitter', 'instagram', 'phone',
         'telephone', 'location_ar', 'location_en', 'welcome_message_ar', 'welcome_message_en'];
 }
