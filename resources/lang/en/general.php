1487654e03a3d86937f96dcddffd3bfea97ad031
Algeneralo <obayda.alesawi@gmail.com>
2019-05-20 23:18:24 +0300

    create localization middleware and files

---
+++ resources/lang/en/general.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: algeneral
+ * Date: 5/20/19
+ * Time: 5:18 PM
+ */
+
+return [
+    "nav" => [
+        "home" => "Home",
+        "projects" => "Projects",
+        "programs" => "Programs",
+        "about" => "About",
+        "partners" => "Our partners",
+        "contactUs" => "Contact Us",
+    ],
+    "home" => [
+        "joinUs" => "Join Us",
+        "donate" => "Donate",
+        "donate_message" => "تبرع gggg",
+        "volunteer" => "Volunteer now",
+        "volunteer_message" => " رسالتطوع الان",
+        "welcome" => "Welcome to our organization",
+        "news" => "News and Publications",
+    ],
+    "about" => [
+        "vision" => "Vision",
+        "message" => "Message",
+        "head_message_title" => "Chairman's speech",
+        "directors" => "Board of Directors",
+        "directors_head" => "Chairman of Board of Directors",
+    ],
+    "projects" => [
+        "sections" => "Sections",
+        "doneProjects" => "Completed Projects",
+        "financeProjects" => "Projects for Finance",
+    ],
+    "partners_message" => "All our work has become possible through the commitment and dedication of our valued partners and supporters. An organization that is grateful to its partners and supporters from various levels that we can not all mention here. They deserve, and we share with them all the appreciation and appreciation we get from communities.",
+    "contact" => "Contact <span>US</span>",
+    "contact_info" => "Contact <span>Info</span>",
+    "more" => "more",
+    "lang" => "Language",
+    "name" => "Name",
+    "email" => "Email",
+    "phone" => "Phone number",
+    "subject" => "Subject",
+    "message" => "Message",
+    "send" => "Send",
+    "volunteer" => "Volunteer",
+    "address" => "Address",
+    "duration" => "Duration in months",
+];
