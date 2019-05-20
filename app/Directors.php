5e71896e9d75822fa1115f0eec4828db535594df
Algeneralo <obayda.alesawi@gmail.com>
2019-05-20 14:31:33 +0300

    add image default value

--- app/Directors.php
+++ app/Directors.php
@@ -9,4 +9,9 @@ class Directors extends Model
 {
     use SoftDeletes;
     protected $fillable = ['name_ar', 'name_en', "position_ar", "position_en", 'image', 'type'];
+
+    //add image default value
+    protected $attributes = [
+        "image" => "staff.png",
+    ];
 }
