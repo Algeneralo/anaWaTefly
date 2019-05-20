0ef31446cdc92a6ff65dc5cdd63535dc62d94ac6
Algeneralo <obayda.alesawi@gmail.com>
2019-05-20 15:54:08 +0300

    add checkeditor upload image function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,32 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\Request;
+
 class AdminController extends Controller
 {
+    /**
+     * custom function for uploading images for check editor
+     *
+     * @param Request $request
+     * @return string
+     */
+    public function uploadCKEditorImage(Request $request)
+    {
+        $CKEditor = $request->get('CKEditor');
+        $funcNum = $request->get('CKEditorFuncNum');
+        $message = $url = '';
+        if ($request->hasFile('upload')) {
+
+            $file = $request->file('upload');
+            if ($file->isValid()) {
+                $url = asset('/assets/img/upload/' . $this->uploadImage($file));
+            } else {
+                $message = 'An error occurred while uploading the file.';
+            }
+        } else {
+            $message = 'No file uploaded.';
+        }
+        return '<script>window.parent.CKEDITOR.tools.callFunction(' . $funcNum . ', "' . $url . '", "' . $message . '")</script>';
+    }
 }
