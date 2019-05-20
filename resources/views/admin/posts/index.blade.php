b9e0d67336723ccd1df49ab96f5f0f68c20a5f5c
Algeneralo <obayda.alesawi@gmail.com>
2019-05-20 16:56:39 +0300

    add delete confirmation message

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -36,7 +36,7 @@
                               method="post">
                             @csrf
                             @method('DELETE')
-                            <button class="btn btn-danger" type="submit">حذف</button>
+                            <button id="delete_button" class="btn btn-danger" type="submit">حذف</button>
                         </form>
                     </td>
                 </tr>
@@ -49,4 +49,25 @@
             </tbody>
         </table>
     </div>
+@endsection
+@section('script')
+    <script>
+        $('#delete_button').on('click', function (e) {
+            e.preventDefault();
+            Swal.fire({
+                title: 'هل انت متأكد من الحذف؟',
+                text: "لن تتمكن من استرجاع هذه البيانات عند حذفها",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: 'الغاء',
+                confirmButtonText: 'حذف'
+            }).then((result) => {
+                if (result.value) {
+                    $(this).parent().submit();
+                }
+            })
+        })
+    </script>
 @endsection
